57719a99d7947ad272488ce6dd4834ce78f66ab5
ellagwithington <85177653+ellagwithington@users.noreply.github.com>
2021-11-20 22:15:43 -0500

    Update InventoryReaderTest.java

--- InventoryReaderTest.java
+++ InventoryReaderTest.java
@@ -4,19 +4,25 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.net.*;
 import java.io.*;
+/**
+ * @author ellawithington
+ *
+ */
 class InventoryReaderTest {
 	static HashMap<String, String> data = new HashMap<String, String>();	
 	@Test
 	void test() {
 		data = InventoryReader.readFile("inventory.xml");
-			String x = data.get("stockNumber");
-			String y = data.get("description");
-			System.out.println(x);
-			System.out.println(y);
+		for (String key : data.keySet()) {
+			String x = key;
+			String y = data.get(key);
 			assert y.equals("box");
 			assert x.equals("45");
 			
 		}
 	}
+}
+
+
 
 
