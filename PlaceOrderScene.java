cf24f0a835eb3eed0ca4d1b5db6a7df5426ec20a
ellagwithington <85177653+ellagwithington@users.noreply.github.com>
2021-11-20 19:53:20 -0500

    Update PlaceOrderScene.java

--- PlaceOrderScene.java
+++ PlaceOrderScene.java
@@ -1,5 +1,4 @@
-
-		//Program by Ella Withington 
+//Program by Ella Withington 
 import java.util.HashMap;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
@@ -17,13 +16,15 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.*;
+import java.io.*;
 import javafx.scene.paint.Color;
 
 public class PlaceOrderScene extends SceneBasic {
-	private Button submit = new Button("Submit");
-	private Button cancel = new Button("Cancel");
+	private Button submitButton = new Button("Submit");
+	private Button cancelButton = new Button("Cancel");
 	private GridPane gridPane = new GridPane();
 	private final int FONT_SIZE = 20;
+    public PrintWriter outgoing;
 
 	public PlaceOrderScene() {
         super("Ordering");
@@ -32,9 +33,9 @@ public class PlaceOrderScene extends SceneBasic {
         gridPane.setPadding(new Insets(10, 10, 10, 10)); 
         gridPane.setVgap(5); 
         gridPane.setHgap(5);  
-        Label userLabel = new Label("Username");
+        Label userLabel = new Label("Stock #");
         userLabel.setFont(new Font(FONT_SIZE));
-        Label accountLabel = new Label("Account");
+        Label accountLabel = new Label("Description");
         accountLabel.setFont(new Font(FONT_SIZE));
         gridPane.add(userLabel, 0, 0);
         gridPane.add(accountLabel, 1, 0);
@@ -46,58 +47,56 @@ public class PlaceOrderScene extends SceneBasic {
         submitButton.setMinWidth(WIDTH);
         root.getChildren().addAll(submitButton);
         root.getChildren().addAll(cancelButton);
-        submitButton.setOnAction(e -> sendOrder);
+        submitButton.setOnAction(e -> sendOrder());
         cancelButton.setOnAction(e -> SceneManager.setCustomerScene());
+        
+
 	}
 	
-    
-    public static HashMap<String,String> getInventory(){
-        HashMap<String, String> data = new HashMap<String, String>();
-        
-        //read inventory
-	System.out.println(filename);
-    File dataFile = new File(filename);
-    if ( ! dataFile.exists() ) {
-        System.out.println("No data file found.");
-        System.exit(1);
-    }
-    try( Scanner scanner = new Scanner(dataFile) ) {
-        while (scanner.hasNextLine()) {
-            String input = scanner.nextLine();
-            int separatorPosition = input.indexOf('<');
-            int separatorPosition2 = input.indexOf('>');
-            input = input.substring(separatorPosition + 1, separatorPosition2);
-            if(input.equals("stockNumber")){
-                String value = scanner.nextLine();
-                value = value.trim();
-                data.put("stockNumber", value);
-            }
-            if(input.equals("description")){
-                String value = scanner.nextLine();
-                value = value.trim();
-                data.put("description", value);
-            }
-        }
-    }
-    catch (IOException e) {
-        System.out.println("Error in data file.");
-        System.exit(1);
-    }
-    for (String key : data.keySet()) {
-        System.out.print("Key = " + key + ", ");
-        System.out.println("Data = " + data.get(key));
-        }
-    return data;
-}
-}
+    public void getInventory() {
+        try{
+        Socket connection = SceneManager.getSocket(); // Server socket
+	    	PrintWriter outgoing;   // Stream for sending data.
+			outgoing = new PrintWriter( connection.getOutputStream() );
+			System.out.println("Sending... INVENTORY");
+            String message = "INVENTORY";
+			outgoing.println(message);
+			outgoing.flush();
+            
+            BufferedReader incoming;
+            incoming = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	        System.out.println("Waiting for inventory..."); // For debugging
+	        String key = incoming.readLine();
+            int row = 1; // Gridepane row index
+            while(key != null ){
+                Label userLabel2 = new Label(key);
+	        	userLabel2.setFont(new Font(FONT_SIZE));
+	            gridPane.add(userLabel2, 0, row);
 
+	            // Add account type
+		        String value = incoming.readLine();
+	            Label accountLabel2 = new Label(value);
+	            accountLabel2.setFont(new Font(FONT_SIZE));
+	            gridPane.add(accountLabel2, 1, row);
+                key = incoming.readLine();
+                value = incoming.readLine();
+            }
+        }
+        
+    catch (Exception e) {
+            System.out.println("Error:  " + e);
+        }
+        
+        
+        
+  
+}
 
 	public void sendOrder() {
 		try {
 			Socket connection = SceneManager.getSocket(); // Server socket
-	    	PrintWriter outgoing;   // Stream for sending data.
-			outgoing = new PrintWriter( connection.getOutputStream() );
-			System.out.println("Sending... INVENTORY");
+	    	PrintWriter outgoing = new PrintWriter( connection.getOutputStream() );
+			System.out.println("Sending... ACCOUNT_LIST");
 			outgoing.println("ACCOUNT_LIST");
 			outgoing.flush();
 //			outgoing.close(); // CAUSES SOCKET TO CLOSE?
@@ -132,4 +131,3 @@ public class PlaceOrderScene extends SceneBasic {
 	}
     
 }
-
